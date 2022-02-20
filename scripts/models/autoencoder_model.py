61da934a65b38d6378b81e45b4b6919d060c772f
manos <manos.mark@gmail.com>
2022-02-20 01:06:41 +0100

    add autoencoder model

---
+++ scripts/models/autoencoder_model.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 19 21:01:33 2022
+
+@author: manos
+"""
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Activation, MaxPool2D, Concatenate
+
+#Convolutional block to be used in autoencoder and U-Net
+def conv_block(input, num_filters):
+    x = Conv2D(num_filters, 3, padding="same")(input)
+    x = BatchNormalization()(x)   #Not in the original network. 
+    x = Activation("relu")(x)
+
+    x = Conv2D(num_filters, 3, padding="same")(x)
+    x = BatchNormalization()(x)  #Not in the original network
+    x = Activation("relu")(x)
+
+    return x
+
+#Encoder block: Conv block followed by maxpooling
+def encoder_block(input, num_filters):
+    x = conv_block(input, num_filters)
+    p = MaxPool2D((2, 2))(x)
+    return x, p   
+
+#Decoder block for autoencoder (no skip connections)
+def decoder_block(input, num_filters):
+    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    x = conv_block(x, num_filters)
+    return x
+
+#Encoder will be the same for Autoencoder and U-net
+#We are getting both conv output and maxpool output for convenience.
+#we will ignore conv output for Autoencoder. It acts as skip connections for U-Net
+def build_encoder(input_image):
+    #inputs = Input(input_shape)
+
+    s1, p1 = encoder_block(input_image, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
+    
+    encoded = conv_block(p4, 1024) #Bridge
+    
+    return encoded
+
+#Decoder for Autoencoder ONLY. 
+def build_decoder(encoded):
+    d1 = decoder_block(encoded, 512)
+    d2 = decoder_block(d1, 256)
+    d3 = decoder_block(d2, 128)
+    d4 = decoder_block(d3, 64)
+    
+    decoded = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
+    return decoded
+
+#Use encoder and decoder blocks to build the autoencoder. 
+def build_autoencoder(input_shape):
+    input_img = Input(shape=input_shape)
+    autoencoder = Model(input_img, build_decoder(build_encoder(input_img)))
+    return(autoencoder)
