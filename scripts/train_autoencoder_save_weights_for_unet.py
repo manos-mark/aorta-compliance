963820e7e0571a3290b4b58f4f999f67284e3a92
manos <manos.mark@gmail.com>
2022-03-04 22:30:44 +0100

    update autoencoder

--- scripts/train_autoencoder_save_weights_for_unet.py
+++ scripts/train_autoencoder_save_weights_for_unet.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     dataset_path = os.path.join('..', 'dataset')
     
     images = sorted(glob('../dataset/diana_segmented/**/*.IMA', recursive=True))
-    images = images[:500]
+    images = images[:100]
     # images += sorted(glob('../dataset/aorte_segmented/**/*.ima', recursive=True))
     # images += sorted(glob('../dataset/marfan_segmented/**/*.ima', recursive=True))
     
@@ -80,7 +80,7 @@ if __name__ == "__main__":
         img_data.append(x)
         
     img_array = np.reshape(img_data, (len(img_data), W, H, 1))
-    # img_array = img_array.astype('float32') / 255.
+    img_array = img_array.astype('float32') / 255.
     
     # dataset = tf_dataset(images, batch=1)
     
