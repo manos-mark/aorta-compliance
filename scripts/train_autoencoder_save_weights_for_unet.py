d70b34115c0c61b1415ad99f87b12839f2a3fdbc
manos <manos.mark@gmail.com>
2022-03-04 16:07:40 +0100

    update autoencoder

--- scripts/train_autoencoder_save_weights_for_unet.py
+++ scripts/train_autoencoder_save_weights_for_unet.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     dataset_path = os.path.join('..', 'dataset')
     
     images = sorted(glob('../dataset/diana_segmented/**/*.IMA', recursive=True))
-    images = images[:10]
+#    images = images[:100]
     #images += sorted(glob('../dataset/aorte_segmented/**/*.ima', recursive=True))
     #images += sorted(glob('../dataset/marfan_segmented/**/*.ima', recursive=True))
     
