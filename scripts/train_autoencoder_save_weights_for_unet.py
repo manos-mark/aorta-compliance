f560b900576fb5936674adfed4abe2e736b85c6f
manos <manos.mark@gmail.com>
2022-03-04 16:05:42 +0100

    update autoencoder

--- scripts/train_autoencoder_save_weights_for_unet.py
+++ scripts/train_autoencoder_save_weights_for_unet.py
@@ -19,65 +19,87 @@ from matplotlib import pyplot as plt
 from glob import glob
 from tqdm import tqdm
 import pydicom
-from preprocessing import crop_and_pad
+from preprocessing import crop_and_pad, contrast_stretching
 from models.autoencoder_model import build_autoencoder, build_encoder
 from models.unet_model import build_unet
 import tensorflow as tf
+import pydicom as dicom
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-SIZE=256
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+H=W=256
 
-
-""" Read images """
-dataset_path = os.path.join('..', 'dataset')
-
-images = sorted(glob('../dataset/diana_segmented/**/*.IMA', recursive=True))
-#images += sorted(glob('../dataset/aorte_segmented/**/*.ima', recursive=True))
-#images += sorted(glob('../dataset/marfan_segmented/**/*.ima', recursive=True))
-
-
-img_data=[]
-for i in tqdm(images):
-    dcm = pydicom.dcmread(i)
+def read_image(path):
+    dcm = dicom.dcmread(path)
     x = dcm.pixel_array
-    x = crop_and_pad(x, SIZE, SIZE)
+    x = contrast_stretching(x)
+    x = crop_and_pad(x, W, H)
     x = x/np.max(x)
     x = x.astype(np.float32)
-    img_data.append(x)
+    x = np.expand_dims(x, axis=-1)
+    return x
+
+def tf_parse(x):
+    def _parse(x, y):
+        x = x.decode()
+        x = read_image(x)
+        return x, x
+
+    x, y = tf.numpy_function(_parse, [x, x], [tf.float32, tf.float32])
+    x.set_shape([H, W, 1])
+    y.set_shape([H, W, 1])
     
-img_array = np.reshape(img_data, (len(img_data), SIZE, SIZE, 1))
-# img_array = img_array.astype('float32') / 255.
+    return x, y
 
-""" Define the autoencoder model """
-autoencoder_model=build_autoencoder((SIZE, SIZE, 1))
-autoencoder_model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-print(autoencoder_model.summary())
-
-""" Train the autoencoder """
-history = autoencoder_model.fit(img_array, img_array,
-        epochs=100, verbose=1)
-
-autoencoder_model.save('autoencoder.h5')
-
-""" Test on a few images """       
-num=random.randint(0, len(img_array)-1)
-test_img = np.expand_dims(img_array[num], axis=0)
-pred = autoencoder_model.predict(test_img)
-
-plt.subplot(1,2,1)
-plt.imshow(test_img[0])
-plt.title('Original')
-plt.subplot(1,2,2)
-plt.imshow(pred[0].reshape(SIZE,SIZE,3))
-plt.title('Reconstructed')
-plt.show()
+def tf_dataset(X, batch=1):
+    dataset = tf.data.Dataset.from_tensor_slices((X))
+    dataset = dataset.cache()
+    dataset = dataset.shuffle(buffer_size=200)
+    dataset = dataset.map(tf_parse)
+    dataset = dataset.batch(batch)
+    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+    
+    return dataset
 
 
-""" Set weights to encoder part of the U-net (first 35 layers) """
-unet_model = build_unet((SIZE, SIZE, 1))
-
-for l1, l2 in zip(unet_model.layers[:35], autoencoder_model.layers[0:35]):
-    l1.set_weights(l2.get_weights())
-
-unet_model.save('unet_pretrained.h5')
+if __name__ == "__main__":
+    """ Read images """
+    dataset_path = os.path.join('..', 'dataset')
+    
+    images = sorted(glob('../dataset/diana_segmented/**/*.IMA', recursive=True))
+    images = images[:10]
+    #images += sorted(glob('../dataset/aorte_segmented/**/*.ima', recursive=True))
+    #images += sorted(glob('../dataset/marfan_segmented/**/*.ima', recursive=True))
+    
+    
+#    img_data=[]
+#    for i in tqdm(images):
+#        dcm = pydicom.dcmread(i)
+#        x = dcm.pixel_array
+#        x = crop_and_pad(x, SIZE, SIZE)
+#        x = x/np.max(x)
+#        x = x.astype(np.float32)
+#        img_data.append(x)
+        
+#    img_array = np.reshape(img_data, (len(img_data), SIZE, SIZE, 1))
+    # img_array = img_array.astype('float32') / 255.
+    
+    dataset = tf_dataset(images, batch=1)
+    
+    """ Define the autoencoder model """
+    autoencoder_model=build_autoencoder((W, H, 1))
+    autoencoder_model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+    print(autoencoder_model.summary())
+    
+    """ Train the autoencoder """
+    history = autoencoder_model.fit(dataset,
+            epochs=100, verbose=1)
+    
+    autoencoder_model.save('autoencoder.h5')
+    
+    """ Set weights to encoder part of the U-net (first 35 layers) """
+    unet_model = build_unet((W, H, 1))
+    
+    for l1, l2 in zip(unet_model.layers[:35], autoencoder_model.layers[0:35]):
+        l1.set_weights(l2.get_weights())
+    
+    unet_model.save('unet_pretrained.h5')
