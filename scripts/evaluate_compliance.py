3494fa76ef560153c8746d2c747e2c2e98e69f21
manos-mark <manos-mark@hotmail.com>
2022-03-16 16:42:25 +0100

    add bland altman plots

--- scripts/evaluate_compliance.py
+++ scripts/evaluate_compliance.py
@@ -6,8 +6,8 @@ Created on Fri Feb 25 11:18:03 2022
 """
 from preprocessing import crop_and_pad
 from metrics import dice_loss, dice_coef, iou, hausdorff
-from train import read_image
-from utils import create_dir
+from train import read_image, read_mask
+from utils import create_dir, bland_altman_plot
 
 from tensorflow.keras.utils import CustomObjectScope
 from natsort import natsorted
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import pyCompare
 import math
 import glob
 import cv2
@@ -84,24 +85,32 @@ if __name__ == '__main__':
     
     """ File paths """
     excel_path = os.path.join('..', 'dataset', 'Diana_Compliance_Dec2020.xlsx')
-    DATASET_FOLDER_PATH = os.path.join('..', 'dataset', 'diana_corrected')
+    DATASET_FOLDER_PATH = os.path.join('..', 'dataset', 'diana_segmented')
     patient_ids = os.listdir(DATASET_FOLDER_PATH)
 
     """ Loading model """
     with CustomObjectScope({'iou': iou, 'dice_coef': dice_coef, 'dice_loss': dice_loss, 'hausdorff': hausdorff}):
         model = tf.keras.models.load_model(os.path.join('..', "output", EXPERIMENT, "model.h5"), compile=False)    
     
+    predicted_compliances, original_compliances = ([] for i in range(2))
+    original_min_areas, predicted_min_areas, original_max_areas, predicted_max_areas = ([] for i in range(4))
     """ Iterate through every patient """
-    for patient_id in patient_ids:
+    for patient_id in patient_ids[4:6]:
         patient_folder_path = os.path.join(DATASET_FOLDER_PATH, patient_id)  
         patient_output_folder_path = os.path.join('..', 'results', patient_id)
+        masks_output_folder_path = os.path.join('..', 'results', patient_id, 'masks')
         create_dir(patient_output_folder_path)
+        create_dir(masks_output_folder_path)
     
         """ Loading patient images """
         image_paths = glob.glob(f'{patient_folder_path}/**/*.IMA', recursive=True)
      
+        """ Loading patient predicted masks """
+        masks = glob.glob(f'{masks_output_folder_path}/**/*.png', recursive=True)
+        
         """ Fetch compliance from excel file """
         original_compliance, original_min, original_max, resolution = compute_compliance_from_excel(patient_id, excel_path)
+        original_compliances.append(original_compliance)
         
         """ Fetch systolic and distolic pressures from excel file """
         syst_press = fetch_syst_press_from_excel(patient_id, excel_path)
@@ -111,16 +120,31 @@ if __name__ == '__main__':
         area_per_slice = []
         
         rows = int(math.ceil(len(image_paths)/5))
-        fig, axs = plt.subplots(rows, 5, figsize=(20,20))
+        fig, axs = plt.subplots(rows, 5, figsize=(30,30))
 
         i = j = 0
-        for image_path in tqdm(image_paths, total=len(image_paths)):  
+        for k, image_path in enumerate(tqdm(image_paths, total=len(image_paths))):  
+            mask_name = image_path.split('/')[3] + '_' + image_path.split('/')[5]
+            mask_name = mask_name + '_' + str(k) + '.png'
+            
             image = read_image(image_path)
             W,H = ((dcmread(image_path)).pixel_array).shape
             
-            """ Segment aorta """
-            aorta = segment_aorta(model, image)
-            aorta = crop_and_pad(aorta[:,:,0], W, H)
+            """ Segment aorta if it's not already segmented """
+            if len(masks) == 0: # TODO make this len(masks) != len(image_paths)
+                                # now it will give error because we have multiple
+                                # scans for each patient
+                aorta = segment_aorta(model, image)
+                aorta = crop_and_pad(aorta[:,:,0], W, H)
+                img = np.zeros((aorta.shape[0], aorta.shape[1], 3))
+                img[:,:,0] = img[:,:,1] = img[:,:,2] = aorta[:,:]
+                plt.imsave(os.path.join(masks_output_folder_path, mask_name), img, cmap='gray')
+            
+            else:
+                aorta = read_mask(os.path.join(masks_output_folder_path, mask_name))
+                aorta = crop_and_pad(aorta[:,:,0], W, H)
+                
+            
             image = crop_and_pad(image[:,:,0], W, H)
             
             axs[i,j].imshow(image, cmap='gray')
@@ -138,8 +162,9 @@ if __name__ == '__main__':
             area = int(area * resolution * resolution)
             area_per_slice.append(area)
             fig.tight_layout()
-        # plt.show()
+#        plt.show()
         fig.savefig(os.path.join(patient_output_folder_path, 'predicted_ROIs.jpg' ))
+        plt.clf()
         
         """ Plot area over time """
         fig = plt.figure(figsize=(15,15))
@@ -148,16 +173,22 @@ if __name__ == '__main__':
         plt.xlabel('Slices')
         plt.ylabel('Area')
         plt.ylim(np.min(area_per_slice)-100, np.max(area_per_slice)+100)
-        # plt.show()
+#        plt.show()
         plt.savefig(os.path.join(patient_output_folder_path, 'predicted_area_over_time.jpg' ))
+        plt.clf()
         
         
         """ Get the minimum and maximum areas across all slices """        
         min_area = min(area_per_slice)
         max_area = max(area_per_slice)
         
+        original_min_areas.append(original_min)
+        original_max_areas.append(original_max)
+        predicted_min_areas.append(min_area)
+        predicted_max_areas.append(max_area)
+        
         print('\nOriginal min, max areas: ', original_min, original_max)
-        print('Predicted min, max areas: ', min_area, max_area, '\t with std: ', np.std(area_per_slice))
+        print('Predicted min, max areas: ', min_area, max_area)
         
         # """ Get the median of 5 values close to minimum and maximum areas across all slices """
         # area_per_slice.sort()
@@ -165,9 +196,17 @@ if __name__ == '__main__':
         # max_area = np.median(np.array(area_per_slice[5:]))
         
         """ Compute global ascending compliance """
-        computed_compliance = compute_compliance(min_area, max_area, syst_press, diast_press)
+        predicted_compliance = compute_compliance(min_area, max_area, syst_press, diast_press)
+        predicted_compliances.append(predicted_compliance)
             
         print('Original ascending compliance', original_compliance)
-        print('Predicted ascending compliance', computed_compliance)
+        print('Predicted ascending compliance', predicted_compliance)
         
         print('\n ========================================================================== \n')
+    
+    pyCompare.blandAltman(original_compliances, predicted_compliances)
+    plt.clf()
+    pyCompare.blandAltman(original_min_areas, predicted_min_areas)
+    plt.clf()
+    pyCompare.blandAltman(original_max_areas, predicted_max_areas)
+    plt.clf()
--- scripts/utils.py
+++ scripts/utils.py
@@ -6,7 +6,7 @@ from glob import glob
 from preprocessing import contrast_stretching, crop_and_pad, bm3d_denoising
 import tensorflow as tf
 import cv2
-import albumentations
+import matplotlib.pyplot as plt
 from albumentations import (
     Compose, RandomBrightness, JpegCompression, HueSaturationValue, RandomContrast, HorizontalFlip,
     Rotate, Affine
@@ -124,3 +124,18 @@ def tf_dataset(X, Y, batch=2, augment=False):
     dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
     
     return dataset
+
+def bland_altman_plot(data1, data2, *args, **kwargs):
+    data1     = np.asarray(data1)
+    data2     = np.asarray(data2)
+    mean      = np.mean([data1, data2], axis=0)
+    diff      = data1 - data2                   # Difference between data1 and data2
+    md        = np.mean(diff)                   # Mean of the difference
+    sd        = np.std(diff, axis=0)            # Standard deviation of the difference
+
+    plt.scatter(mean, diff, *args, **kwargs)
+    plt.axhline(md,           color='gray', linestyle='--')
+    plt.axhline(md + 1.96*sd, color='gray', linestyle='--')
+    plt.axhline(md - 1.96*sd, color='gray', linestyle='--')
+    plt.title('Bland-Altman Plot')
+    plt.show()
