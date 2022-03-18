7e10fd721f0cb27558753af03845929d30b3d54f
manos-mark <manos-mark@hotmail.com>
2022-03-18 16:25:40 +0100

    experiment on att & res unet

--- scripts/eval.py
+++ scripts/eval.py
@@ -59,7 +59,7 @@ def interpret_training_results():
 """ Global parameters """
 H = 256
 W = 256
-EXPERIMENT = 'unet-diana-lr_0.0001-batch_8-augmented'
+EXPERIMENT = 'composed_dataset/unet_lr_0.001-batch_8-dice_loss-augmented-multi-centre-shuffle_data'
 OUTPUT_FOLDER_PATH = os.path.join('..', 'results', EXPERIMENT)
 
 if __name__ == "__main__":
